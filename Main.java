ef86a999302767c66aedd6663fc629d5446262a6
Pierre Lourdelet <56489410+Tarichikato@users.noreply.github.com>
2019-12-13 20:33:29 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -16,18 +16,18 @@ public class Main {
     	
     	//Definition des transactions
 
-        Transaction transaction1 = new Transaction("Manu", "IMT BS", 2019L);
-        Transaction transaction2 = new Transaction("Rudy", "IngÈnieur IPParis", 2022L);
-        Transaction transaction3 = new Transaction("Victor", "IngÈnieur TSP", 2020L);
-        Transaction transaction4 = new Transaction("Lyticia", "IngÈnieur IMT Lille Douai", 2022L);
-        Transaction transaction5 = new Transaction("Rudy", "50 mËtres brasse", 2010L);
-        Transaction transaction6 = new Transaction("Pierre", "IngÈnieur IPParis", 2022L);
-        Transaction transaction7 = new Transaction("Valentin", "100 mËtres brasse", 2010L);
-        Transaction transaction8 = new Transaction("Raph", "IngÈnieur IPParis", 2022L);
-        Transaction transaction9 = new Transaction("Lamia", "IngÈnieur IPParis", 2022L);
-        Transaction transaction10 = new Transaction("Gougou", "IngÈnieur ECEParis", 2022L);
-        Transaction transaction11 = new Transaction("Karolina", "Responsable KryptosphËre Paris", 2019L);
-        Transaction transaction12 = new Transaction("Louis", "VP KryptosphËre Paris", 2019L);
+        Transaction transaction1 = new Transaction("Manu", "Meilleur responsable d'antenne Kryptosph√®re", 2019L);
+        Transaction transaction2 = new Transaction("Rudy", "Ing√©nieur IPParis", 2022L);
+        Transaction transaction3 = new Transaction("Victor", "Ing√©nieur TSP", 2020L);
+        Transaction transaction4 = new Transaction("Lyticia", "Ing√©nieur IMT Lille Douai", 2022L);
+        Transaction transaction5 = new Transaction("Rudy", "50 m√®tres brasse", 2010L);
+        Transaction transaction6 = new Transaction("Pierre", "Ing√©nieur IPParis", 2022L);
+        Transaction transaction7 = new Transaction("Valentin", "100 m√®tres brasse", 2010L);
+        Transaction transaction8 = new Transaction("Raph", "Ing√©nieur IPParis", 2022L);
+        Transaction transaction9 = new Transaction("Lamia", "Ing√©nieur IPParis", 2022L);
+        Transaction transaction10 = new Transaction("Gougou", "Ing√©nieur ECEParis", 2022L);
+        Transaction transaction11 = new Transaction("Karolina", "Responsable Kryptosph√®re Paris", 2019L);
+        Transaction transaction12 = new Transaction("Louis", "VP Kryptosph√®re Paris", 2019L);
         
         //On regroupe les transactions dans des tableaux
         
@@ -37,13 +37,13 @@ public class Main {
         Transaction transactionsBlock4[] = {transaction10,transaction11,transaction12};
         
         /*
-        CrÈation d'une "liste chainÈe" (Ce n'est pas la Blockchain elle ne sert qu'a l"affichage des transactions)
-        Pour comprendre son rÙle aller voir dans le fichier Chain.java
+        Cr√©ation d'une "liste chain√©e" (Ce n'est pas la Blockchain elle ne sert qu'a l"affichage des transactions)
+        Pour comprendre son r√¥le aller voir dans le fichier Chain.java
         */
         
         Chain Degree = new Chain();
         
-        //CrÈation des diffÈrents blocks
+        //Cr√©ation des diff√©rents blocks
          
         Block firstBlock = new Block(0, transactionsBlock1) ;        
         Block secondBlock = new Block(firstBlock.hashCode(), transactionsBlock2);      
@@ -51,14 +51,14 @@ public class Main {
         Block fourthBlock = new Block(thirdBlock.hashCode(), transactionsBlock4);
         
         
-        //IntÈgration des blocks dans la "liste chainÈe"
+        //Int√©gration des blocks dans la "liste chain√©e"
         
         Degree.add( firstBlock);
         Degree.add(secondBlock);
         Degree.add(thirdBlock);
         Degree.add(fourthBlock);
         
-        //Affichage des Hash des diffÈrents blocks (Facultatif)
+        //Affichage des Hash des diff√©rents blocks (Facultatif)
         
         System.out.println("first " + firstBlock.hashCode());
         System.out.println("second " + secondBlock.hashCode());
@@ -69,7 +69,7 @@ public class Main {
         //Recherche de personnes dans la Blockchain
 
         Degree.findStudent("Manu");
-        Degree.findDegree("IngÈnieur ECEParis");
+        Degree.findDegree("Ing√©nieur ECEParis");
         Degree.findYear(2019L);
         
     }
